cec70d55d831377e4ac4ad2e465a2c08b4c44d95
kazu833 <tomsfashion833@gmail.com>
2025-01-13 17:34:23 +0900

    users_controller　編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
 
+  before_action :authenticate_user!, except: [:show]
+
 def show
   @user = User.find(params[:id])
-  @prototypes = Prototype.includes(:user)
+  @prototypes = @user.prototypes
 end
 
 end
