9f5ae8ccedc53b89fcbce9fe441a339330f6cce8
kazu833 <tomsfashion833@gmail.com>
2025-01-14 14:27:14 +0900

    修正

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -5,7 +5,7 @@ class Prototype < ApplicationRecord
   has_one_attached :image
 
 
-  #validates :image, presence: true
+  validates :image, presence: true
   validates :title, presence: true
   validates :catch_copy, presence: true
   validates :concept, presence: true
